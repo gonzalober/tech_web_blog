88d516fb179e49712644517704fca5d6180e57a4
Gonzalo Berhouet <gonzalo.berhouet@ecs.co.uk>
2021-10-27 16:41:25 +0100

    shell script in process

--- server/posts.sh
+++ server/posts.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 curl -O "https://academy-project-blogs.s3-eu-west-1.amazonaws.com/teaching_code.doc"
+curl -O "https://academy-project-blogs.s3-eu-west-1.amazonaws.com/IDC.md"
+curl -O "https://academy-project-blogs.s3-eu-west-1.amazonaws.com/milestones.txt"
 
-mkdir blogs| mv "${teaching_code}" ~blogs/
+# Validations on the file size, naming and file extension should be made as part of your shell script.
+
+
+
+psql -U postgres -d tech_blog -c set content `cat teaching_code.doc` "INSERT INTO posts (username, title, content) VALUES ($1, $2, $3)"
