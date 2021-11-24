e63df924c2bee960c63a2cc4c4f3782a90cb8fd4
Gonzalo Berhouet <gonzalo.berhouet@ecs.co.uk>
2021-11-24 12:15:39 +0000

    timestamp

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -1,7 +1,8 @@
 CREATE TABLE posts (
         id serial PRIMARY KEY,
         username VARCHAR ( 50 ) NOT NULL,
-        createdAt TIMESTAMP,
-        updatedAt TIMESTAMP, title VARCHAR (255),
+        createdAt TIMESTAMP ( 50 ) NOT NULL,
+        updatedAt TIMESTAMP ( 50 ) NOT NULL, 
+        title VARCHAR (255),
         content VARCHAR (255) 
 );
