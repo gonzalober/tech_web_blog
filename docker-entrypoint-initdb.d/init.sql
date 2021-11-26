9c2710cb41c310374f79a7f9d05c32f20c92a44b
Gonzalo Berhouet <gonzalo.berhouet@ecs.co.uk>
2021-11-26 08:56:59 +0000

    demo image push to ecr

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -1,8 +1,8 @@
 CREATE TABLE posts (
         id serial PRIMARY KEY,
         username VARCHAR ( 50 ) NOT NULL,
-        createdAt TIMESTAMP ( 50 ) NOT NULL,
-        updatedAt TIMESTAMP ( 50 ) NOT NULL, 
+        createdAt TIMESTAMP DEFAULT Now(),
+        updatedAt TIMESTAMP DEFAULT Now(), 
         title VARCHAR (255),
         content VARCHAR (255) 
 );
